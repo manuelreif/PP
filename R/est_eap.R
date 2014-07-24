1f6425384662a65c09b0287f502a18b05309e084
Manuel Reif <manuel.reif@gmail.com>
2014-07-24 19:23:40 +0200

    EAP estimation for 4PL model

---
+++ R/est_eap.R
@@ -0,0 +1,51 @@
+est_eap <- function(respm, thres, slopes, lowerA=NULL, upperA=NULL,
+                    mu = NULL, sigma2 = NULL)
+{
+  
+  
+#quads <- quadIT(nodes = 90,mu = mu[perp],sigma = sqrt(sigma2[perp]),absrange = 6)
+
+nodmat <- matrix(0,nrow(respm),90)  
+weimat <- matrix(0,nrow(respm),90) 
+# #persons x #nodes
+
+for(qrun in 1:nrow(respm))
+  {
+  qres <- quadIT(nodes = 90,mu = mu[qrun],sigma = sqrt(sigma2[qrun]),absrange = 6)
+  nodmat[qrun,]  <- qres[[1]]$nodes
+  weimat[qrun,]  <- qres[[1]]$weights
+  }
+
+
+FPL_eap <- sapply(1:nrow(respm),function(ep)
+            {
+              
+            oben <- exp(t(outer(nodmat[ep,],thres[-1,],"-"))*slopes)
+            oben2 <- oben * (upperA-lowerA)
+            P <- lowerA + oben2/(1+oben)
+            # item x nodes
+            Q <- 1 - P
+            
+            xi <- respm[ep,]
+            ximi <- 1-xi
+            pxi <- P * xi
+            qxi <- Q * ximi
+            li  <- pxi + qxi
+            
+            Li <- apply(li,2,function(x)prod(x,na.rm=TRUE)) 
+            EAP <- sum(Li * weimat[ep,] * nodmat[ep,])/sum((Li*weimat[ep,]))
+            
+            EAP_v <- sum(Li * weimat[ep,] * (nodmat[ep,]-EAP)^2)/sum((Li*weimat[ep,]))
+            
+            c(EAP,EAP_v)
+            })
+
+
+  
+  
+  
+  
+  
+  
+  
+}
