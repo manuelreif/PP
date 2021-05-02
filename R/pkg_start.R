0dd55834f7eb7faab47f25d5968fa25373cbfb02
Jan Steinfeld <jan.d.steinfeld@gmail.com>
2021-05-02 10:51:53 +0200

    updated start message

--- R/pkg_start.R
+++ R/pkg_start.R
@@ -1,5 +1,5 @@
 
 .onAttach <- function(libname, pkgname)
 {
-  packageStartupMessage(paste(pkgname)," package calling ...\nFollow this project on github: https://github.com/manuelreif/PP.git")
+  packageStartupMessage(paste(pkgname)," package calling ...\nFollow this project on github: https://github.com/jansteinfeld/PP.git")
 }
